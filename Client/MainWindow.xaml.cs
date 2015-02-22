5a03a02e360f5837202fb8d6f035a526b531a5cc
hschroedl <hansj.schroedl@gmail.com>
2015-02-22 18:56:27 +0100

    Add client base view model and database connection

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Entities.Database;
+using Entities.Entities;
 
-namespace Client {
+namespace Client
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
+            TestConnection();
+        }
+
+        public void TestConnection()
+        {
+            UserAccountContext context = new UserAccountContext();
+            User user = context.Users.First();
+            Console.WriteLine(user.Name);
         }
     }
 }
---
+++ Client/ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Client.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
