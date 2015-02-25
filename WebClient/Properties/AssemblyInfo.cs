0ebf7cd5fdf95153cc1ddaaa6104b683514662c8
hschroedl <hansj.schroedl@gmail.com>
2015-02-25 21:09:53 +0100

    Another web client commit.

--- WebClient/Properties/AssemblyInfo.cs
+++ WebClient/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d72f76d0-4876-48dd-a71d-aa244729613d")]
+[assembly: Guid("2da743d7-d89b-4b5b-8c84-f7d877f9cde9")]
 
 // Version information for an assembly consists of the following four values:
 //
