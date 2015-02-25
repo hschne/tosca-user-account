d026eb17c5d5eec5b730fff421c4f929f7ecc63e
hschroedl <hansj.schroedl@gmail.com>
2015-02-25 21:14:48 +0100

    Add users controller.

--- WebClient/Global.asax.cs
+++ WebClient/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Entities.Database;
 
 namespace WebClient {
     public class MvcApplication : System.Web.HttpApplication {
@@ -13,6 +15,7 @@ namespace WebClient {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new UserAccountInitializer());
         }
     }
 }
